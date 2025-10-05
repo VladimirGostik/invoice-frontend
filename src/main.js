11e94ffd8103f7c2f4970feee5ab612ad632e7c1
Vladimir Gostik <gostikvladko9@gmail.com>
2025-10-05 18:24:16 +0200

    env + packeges

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// CoreUI CSS
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// CoreUI Vue komponenty
+import CoreuiVue from '@coreui/vue'
+
+// Router a Pinia store
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(CoreuiVue)
+
+app.mount('#app')
